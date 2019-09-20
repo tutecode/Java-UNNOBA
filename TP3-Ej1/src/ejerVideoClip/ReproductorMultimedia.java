e1441349ed76e02a577839c8375c274dee4b24bc
tutegomeze <tutegomeze1997@gmail.com>
2019-09-20 16:21:21 -0300

    UNNOBA

---
+++ TP3-Ej1/src/ejerVideoClip/ReproductorMultimedia.java
@@ -0,0 +1,23 @@
+package ejerVideoClip;
+
+public class ReproductorMultimedia implements VideoClip{
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bucle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ TP3-Ej1/src/ejerVideoClip/VideoClip.java
@@ -0,0 +1,12 @@
+package ejerVideoClip;
+
+public interface VideoClip {
+	/*comienza la reproduccion del video*/
+	public void play();
+	
+	/*reproduce el clip en un bucle, un loop infinito*/
+	public void bucle();
+	
+	/*detiene la reproducción del video*/
+	public void stop();
+}
